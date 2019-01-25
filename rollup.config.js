f8df20d31c6bd874b404d5422f44d53add536329
rxliuli <rxliuli@gmail.com>
2019-01-25 14:28:02 +0800

    引入 rollup-plugin-eslint 插件包

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import babel from 'rollup-plugin-babel'
 // 注意，这里引入需要使用 { uglify } 而非 uglify，因为 uglify 导出自身时使用的是 exports.uglify
 import { uglify } from 'rollup-plugin-uglify'
+import { eslint } from 'rollup-plugin-eslint'
 
 export default {
   // 入口文件
@@ -14,6 +15,8 @@ export default {
     format: 'umd'
   },
   plugins: [
+    // 引入 eslint 插件，必须在 babel 之前引入，因为 babel 编译之后的代码未必符合 eslint 规范，eslint 仅针对我们 [原本] 的代码
+    eslint(),
     // 引入 babel 插件
     babel({
       exclude: 'node_modules/**'
